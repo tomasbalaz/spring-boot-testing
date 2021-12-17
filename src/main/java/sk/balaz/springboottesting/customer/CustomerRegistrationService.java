19e486098b5891ff861ffc5ad4f32275546744ec
Tomas Balaz <balaztomas0@gmail.com>
2021-12-17 15:21:17 +0100

    Implemented registerNewCustomer in CustomerRegistrationService

--- src/main/java/sk/balaz/springboottesting/customer/CustomerRegistrationService.java
+++ src/main/java/sk/balaz/springboottesting/customer/CustomerRegistrationService.java
@@ -3,6 +3,8 @@ package sk.balaz.springboottesting.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class CustomerRegistrationService {
 
@@ -13,7 +15,7 @@ public class CustomerRegistrationService {
         this.customerRepository = customerRepository;
     }
 
-    public  void registerNewCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+    public  void registerNewCustomer(CustomerRegistrationRequest request) {
         // 1. phone number is taken
 
         //2. if taken lets check if belongs to same customer
@@ -23,5 +25,18 @@ public class CustomerRegistrationService {
         //2.2 throw an exception
 
         //3. save customer
+
+        String phoneNumber = request.getCustomer().getPhoneNumber();
+        Optional<Customer> customerOptional = customerRepository.selectCustomerByPhoneNumber(phoneNumber);
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            if (customer.getName().equals(request.getCustomer().getName())) {
+                return;
+            }
+            throw new IllegalStateException(String.format("phone number [%s] is taken", phoneNumber));
+        }
+
+        customerRepository.save(request.getCustomer());
+
     }
 }
