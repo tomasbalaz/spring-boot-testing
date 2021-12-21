86597aa37edb47ad3f6dde7daa0605d9fb7b4955
Tomas Balaz <balaztomas0@gmail.com>
2021-12-21 15:27:58 +0100

    Create tests itShouldSelectCustomerByPhoneNumber, itShouldNotSelectCustomerByPhoneNumberWhenNumberDoesNotExist

--- src/test/java/sk/balaz/springboottesting/customer/CustomerRepositoryTest.java
+++ src/test/java/sk/balaz/springboottesting/customer/CustomerRepositoryTest.java
@@ -24,10 +24,37 @@ class CustomerRepositoryTest {
     void itShouldSelectCustomerByPhoneNumber() {
 
         //given - set up of something
+        UUID id = UUID.randomUUID();
+        String phoneNumber = "00000";
+        Customer customer = new Customer(id, "Abel", phoneNumber);
 
         //when - when something is called
+        underTest.save(customer);
 
         //then - assertion
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+        assertThat(optionalCustomer)
+                .isPresent()
+                .hasValueSatisfying(c -> {
+//                    assertThat(c.getId()).isEqualTo(id);
+//                    assertThat(c.getName()).isEqualTo("Abel");
+//                    assertThat(c.getPhoneNumber()).isEqualTo("00");
+
+                    assertThat(c).isEqualToComparingFieldByField(customer);
+                });
+    }
+
+    @Test
+    void itShouldNotSelectCustomerByPhoneNumberWhenNumberDoesNotExist() {
+        //given - set up of something
+        String phoneNumber = "00000";
+
+        //when - when something is called
+        Optional<Customer> optionalCustomer = underTest.selectCustomerByPhoneNumber(phoneNumber);
+
+        //then - assertion
+        assertThat(optionalCustomer)
+                .isNotPresent();
     }
 
     @Test
