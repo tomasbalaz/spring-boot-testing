22722e3eacf513b2ca7074644b61b50242a62ca7
Tomas Balaz <balaztomas0@gmail.com>
2022-01-11 09:36:35 +0100

    Created itShouldChargeCard test in StripeServiceTest, add dependency to com.google.code.gson.gson

---
+++ src/test/java/sk/balaz/springboottesting/stripe/StripeServiceTest.java
@@ -0,0 +1,71 @@
+package sk.balaz.springboottesting.stripe;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import com.stripe.net.RequestOptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import sk.balaz.springboottesting.payment.CardPaymentCharge;
+import sk.balaz.springboottesting.payment.Currency;
+import sk.balaz.springboottesting.payment.stripe.StripeApi;
+import sk.balaz.springboottesting.payment.stripe.StripeService;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+class StripeServiceTest {
+
+    private StripeService underTest;
+
+    @Mock
+    private StripeApi stripeApi;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        underTest = new StripeService(stripeApi);
+    }
+
+    @Test
+    void itShouldChargeCard() throws StripeException {
+        //given
+        String cardSource = "0x0x0x";
+        BigDecimal amount = new BigDecimal("10");
+        Currency currency = Currency.USD;
+        String description = "Donate";
+
+        Charge charge = new Charge();
+        charge.setPaid(true);
+        given(stripeApi.create(anyMap(), any())).willReturn(charge);
+
+        //when
+        CardPaymentCharge cardPaymentCharge = underTest.chargeCard(cardSource, amount, currency, description);
+
+        //then
+        ArgumentCaptor<Map<String, Object>> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+        ArgumentCaptor<RequestOptions> requestOptionsArgumentCaptor = ArgumentCaptor.forClass(RequestOptions.class);
+
+        then(stripeApi).should().create(mapArgumentCaptor.capture(), requestOptionsArgumentCaptor.capture());
+
+        Map<String, Object> requestMap = mapArgumentCaptor.getValue();
+        assertThat(requestMap.keySet()).hasSize(4);
+        assertThat(requestMap.get("amount")).isEqualTo(amount);
+        assertThat(requestMap.get("currency")).isEqualTo(currency);
+        assertThat(requestMap.get("source")).isEqualTo(cardSource);
+        assertThat(requestMap.get("description")).isEqualTo(description);
+
+        RequestOptions requestOptions = requestOptionsArgumentCaptor.getValue();
+        assertThat(requestOptions).isNotNull();
+
+        assertThat(cardPaymentCharge.isCarDebited()).isTrue();
+    }
+}
