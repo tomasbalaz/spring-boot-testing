1543602d81352e2b7335701e32e4903a0932ab0d
Tomas Balaz <balaztomas0@gmail.com>
2022-01-18 15:10:08 +0100

    Created itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13, itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign tests

--- src/main/java/sk/balaz/springboottesting/utils/PhoneNumberValidator.java
+++ src/main/java/sk/balaz/springboottesting/utils/PhoneNumberValidator.java
@@ -5,7 +5,8 @@ import java.util.function.Predicate;
 public class PhoneNumberValidator implements Predicate<String> {
 
     @Override
-    public boolean test(String s) {
-        return false;
+    public boolean test(String phoneNumber) {
+        return phoneNumber.startsWith("+44") &&
+                phoneNumber.length() == 13;
     }
 }
--- src/test/java/sk/balaz/springboottesting/utils/PhoneNumberValidatorTest.java
+++ src/test/java/sk/balaz/springboottesting/utils/PhoneNumberValidatorTest.java
@@ -1,6 +1,7 @@
 package sk.balaz.springboottesting.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,11 +18,35 @@ public class PhoneNumberValidatorTest {
     @Test
     void itShouldValidatePhoneNumber() {
         //given
-         String phoneNumber = "+447000000";
+         String phoneNumber = "+447000000000";
         //when
         boolean isValid = underTest.test(phoneNumber);
 
         //then
         assertThat(isValid).isTrue();
+    }
+
+    @Test
+    @DisplayName("Should fail when length is bigger than 13")
+    void itShouldValidatePhoneNumberWhenIncorrectAndHasLengthBiggerThan13() {
+        //given
+        String phoneNumber = "+447000000000884848";
+        //when
+        boolean isValid = underTest.test(phoneNumber);
+
+        //then
+        assertThat(isValid).isFalse();
+    }
+
+    @Test
+    @DisplayName("Should fail when it does not start with +")
+    void itShouldValidatePhoneNumberWhenDoesNotStartWithPlusSign() {
+        //given
+        String phoneNumber = "447000000000884848";
+        //when
+        boolean isValid = underTest.test(phoneNumber);
+
+        //then
+        assertThat(isValid).isFalse();
     }
 }
