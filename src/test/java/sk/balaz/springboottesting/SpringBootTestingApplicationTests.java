8c19b2ba7e55a025154ed34cfd07a6d51ea4f5ce
Tomas Balaz <balaztomas0@gmail.com>
2022-01-21 11:14:52 +0100

    Add basic starting point urls

--- src/test/java/sk/balaz/springboottesting/SpringBootTestingApplicationTests.java
+++ src/test/java/sk/balaz/springboottesting/SpringBootTestingApplicationTests.java
@@ -6,6 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringBootTestingApplicationTests {
 
+    // Mockito starting point
+    //https://javadoc.io/doc/org.mockito/mockito-core/latest/index.html
+
+    // Power mock starting point
+    // https://github.com/powermock/powermock
+
     @Test
     void contextLoads() {
     }
